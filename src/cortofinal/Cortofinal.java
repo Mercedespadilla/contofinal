e040134fb525b062fcb22857f02753c0fd137fd2
LN710Q <LN710Q@DEILABO1-PC029>
2018-06-25 12:15:56 -0600

    cambios en el main

--- src/cortofinal/Cortofinal.java
+++ src/cortofinal/Cortofinal.java
@@ -5,6 +5,8 @@
  */
 package cortofinal;
 
+import consultas.Consultas;
+
 /**
  *
  * @author LN710Q
@@ -15,7 +17,7 @@ public class Cortofinal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Consultas gui= new Consultas();
     }
     
 }
